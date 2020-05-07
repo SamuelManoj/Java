47bd0c31ea1bfcab8904e317abd6727f6e17e0db
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-07 17:22:26 +0530

    Create Machine.java

---
+++ core_java_basics/Inheritance/Machine.java
@@ -0,0 +1,14 @@
+package core_java_basics.Inheritance;
+
+public class Machine {
+	String name = "Amaze";
+
+	public void start() {
+		System.out.println("Machine Started");
+	}
+
+	public void stop() {
+		System.out.println("Machine stopped");
+	}
+
+}
