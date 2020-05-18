d6f9a6f2dc1ffd5bf7f99ef57aece73e8507f045
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-18 13:56:04 +0530

    Create Machine.java
    Created 2 classes with implements

---
+++ core_java_basics/interfaces/Machine.java
@@ -0,0 +1,11 @@
+package core_java_basics.interfaces;
+
+public class Machine implements Info {
+	public void start() {
+		System.out.println("machine started");
+	}
+
+	public void display() {
+		System.out.println("Machine info started");
+	}
+}
