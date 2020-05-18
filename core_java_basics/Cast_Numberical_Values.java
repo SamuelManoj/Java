435b155f4ae9eec1f47ae19e2bc70cd1c2ec8b5e
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-18 14:19:58 +0530

    Create Cast_Numberical_Values.java

---
+++ core_java_basics/Cast_Numberical_Values.java
@@ -0,0 +1,22 @@
+package core_java_basics;
+
+public class Cast_Numberical_Values {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte by = 12;
+		short sh = 123;
+		int a = 1122;
+		long b = 1122334;
+
+		float c = 2.3f;
+		double d = 2.3;
+
+		d = a;
+		System.out.println(d);
+
+		a = (int) c;
+		System.out.println(a);
+	}
+
+}
