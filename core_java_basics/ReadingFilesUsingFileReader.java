2b41631e1e5817d8999c8fee9f236ae2d8de712d
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-19 13:36:58 +0530

    Create ReadingFilesUsingFileReader.java

---
+++ core_java_basics/ReadingFilesUsingFileReader.java
@@ -0,0 +1,40 @@
+package core_java_basics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadingFilesUsingFileReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("text.txt");
+		BufferedReader br = null;
+		try {
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found " + file.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unable  to read " + file.toString());
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
