3c8d72325bb844392314e373cf7d774f4b96e124
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-19 13:21:16 +0530

    Create Solution.java

---
+++ core_java_basics/abstracts/Solution.java
@@ -0,0 +1,16 @@
+package core_java_basics.abstracts;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child1 child = new Child1();
+		child.setId(1);
+		child.run();
+
+		Child2 ch = new Child2();
+		ch.setId(2);
+		ch.run();
+	}
+
+}
