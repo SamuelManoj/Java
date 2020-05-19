ac3bd801d66a42cffb45acfcf760e2d8faa0a220
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-19 13:22:01 +0530

    Create Child2.java

---
+++ core_java_basics/abstracts/Child2.java
@@ -0,0 +1,17 @@
+package core_java_basics.abstracts;
+
+public class Child2 extends Parent {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		System.out.println("Child2 started");
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Child2 stops");
+	}
+
+}
