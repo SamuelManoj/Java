b7f0c9dab06e083089da1a9de6a64c32a227df52
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-20 15:25:34 +0530

    Create EqualsMethod.java

---
+++ core_java_basics/EqualsMethod.java
@@ -0,0 +1,70 @@
+package core_java_basics;
+
+public class EqualsMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person1 = new Person(1, "Sam");
+		Person person2 = new Person(1, "Sam");
+
+		System.out.println(new Object());
+
+		System.out.println(person1.equals(person2));
+	}
+
+}
+
+class Person {
+	int id;
+	String name;
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
