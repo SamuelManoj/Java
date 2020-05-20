c235a37d833e6cf0d81936c7efc2a1719244a8cf
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-20 13:24:57 +0530

    Update and rename ReadingFilesUsingFileReader.java to FileReadingUsingFileReader.java

--- core_java_basics/FileReadingUsingFileReader.java
+++ core_java_basics/FileReadingUsingFileReader.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadingFilesUsingFileReader {
+public class FileReadingUsingFileReader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
