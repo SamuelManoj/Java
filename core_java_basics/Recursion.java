0ce6f8cc40f20230a49a9e24225c62fbd2a6b5c5
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-20 16:54:24 +0530

    Create Recursion.java

---
+++ core_java_basics/Recursion.java
@@ -0,0 +1,18 @@
+package core_java_basics;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(factorial(7));
+	}
+
+	private static int factorial(int value) {
+
+		if (value == 1) {
+			return 1;
+		}
+		return value * factorial(value - 1);
+	}
+
+}
