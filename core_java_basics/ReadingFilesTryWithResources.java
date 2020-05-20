f163633bad1184f185a49a43015f540e40ed9de6
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-20 12:46:49 +0530

    Create ReadingFilesTryWithResources.java

---
+++ core_java_basics/ReadingFilesTryWithResources.java
@@ -0,0 +1,29 @@
+package core_java_basics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadingFilesTryWithResources {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("text.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found " + file.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unable  to read " + file.toString());
+		}
+	}
+
+}
