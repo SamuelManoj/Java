7d51d41b00cc8581f09cd9f0f2ef950ba8a834e8
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-05-26 13:45:11 +0530

    Create Contact.java

---
+++ core_java_basics/serialization_arrays/Contact.java
@@ -0,0 +1,23 @@
+package core_java_basics.serialization_arrays;
+
+import java.io.Serializable;
+
+public class Contact implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5793489238797014783L;
+	private int id;
+	private String name;
+
+	public Contact(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", name=" + name + "]";
+	}
+}
