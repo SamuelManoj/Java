c7355f958752227b1f8e92ee656c8c3b34b08984
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-06-09 17:37:27 +0530

    Create Solutions.java

---
+++ collections_framework/iterable/Solutions.java
@@ -0,0 +1,14 @@
+package collections_framework.iterable;
+
+public class Solutions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UrlLibrary urlLibrary = new UrlLibrary();
+
+		for (String url : urlLibrary) {
+			System.out.println(url);
+		}
+	}
+
+}
