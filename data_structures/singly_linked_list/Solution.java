a9499de39d62b12042f913c1d1135c23d6e0efcc
Samuel Manoj <Samuel_Manoj@Outlook.com>
2020-07-09 13:38:42 +0530

    Create Solution.java

---
+++ data_structures/singly_linked_list/Solution.java
@@ -0,0 +1,17 @@
+package data_structures.singly_linked_list;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.prepend(1);
+		ll.prepend(2);
+		ll.prepend(3);
+		ll.prepend(4);
+		System.out.println("hekllo");
+		System.out.println(ll);
+		System.out.println("s");
+	}
+
+}
